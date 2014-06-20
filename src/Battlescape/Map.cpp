ea47d6966f64a816b63a200c776e6e2c50027a6a
Bryan P. Whalen <bryanpwhalen@gmail.com>
2014-06-19 22:54:12 -0600

    - Flash the aim cursor red/yellow if you do not have enough TU to continue firing.

--- src/Battlescape/Map.cpp
+++ src/Battlescape/Map.cpp
@@ -887,6 +887,8 @@ void Map::drawTerrain(Surface *surface)
 					{
 						if (_camera->getViewLevel() == itZ)
 						{
+							BattleAction *action = _save->getBattleGame()->getCurrentAction();
+
 							if (_cursorType != CT_AIM)
 							{
 								if (unit && (unit->getVisible() || _save->getDebugMode()))
@@ -896,7 +898,9 @@ void Map::drawTerrain(Surface *surface)
 							}
 							else
 							{
-								if (unit && (unit->getVisible() || _save->getDebugMode()))
+								if(action->actor && action->TU && action->actor->getTimeUnits() < action->TU)
+									frameNumber = _animFrame % 2 ? 6 : 7;
+								else if (unit && (unit->getVisible() || _save->getDebugMode()))
 									frameNumber = 7 + (_animFrame / 2); // yellow animated crosshairs
 								else
 									frameNumber = 6; // red static crosshairs
@@ -907,7 +911,6 @@ void Map::drawTerrain(Surface *surface)
 							// UFO extender accuracy: display adjusted accuracy value on crosshair in real-time.
 							if (_cursorType == CT_AIM && Options::battleUFOExtenderAccuracy)
 							{
-								BattleAction *action = _save->getBattleGame()->getCurrentAction();
 								RuleItem *weapon = action->weapon->getRules();
 								std::ostringstream ss;
 								int accuracy = action->actor->getFiringAccuracy(action->type, action->weapon);
