3cf603406a4b09e0a27de5421f9318a80a4d8f67
Bryan P. Whalen <bryanpwhalen@gmail.com>
2014-06-22 19:34:29 -0600

    - Re-enabled selected unit bobbing for role icons. - New mini icon for sniper role. - Shrunk medic small icon.

--- src/Battlescape/Map.cpp
+++ src/Battlescape/Map.cpp
@@ -1104,7 +1104,7 @@ void Map::drawTerrain(Surface *surface)
 			Surface *roleIcon;
 			if(soldier && (role = soldier->getRole()) && !role->isBlank() && (roleIcon = _game->getResourcePack()->getSurface(role->getRules()->getSmallIconSprite())))
 			{
-				roleIcon->blitNShade(surface, screenPosition.x + offset.x + (_spriteWidth / 2) - (roleIcon->getWidth() / 2), screenPosition.y + offset.y - roleIcon->getHeight(), 0);
+				roleIcon->blitNShade(surface, screenPosition.x + offset.x + (_spriteWidth / 2) - (roleIcon->getWidth() / 2), screenPosition.y + offset.y - roleIcon->getHeight() + arrowBob[_animFrame], 0);
 			}
 			else
 			{
